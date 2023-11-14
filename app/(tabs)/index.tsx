96c5219d594a1ed3186c87fd2f62223880c98874
Pierre VAZ <vaz.pierre57@gmail.com>
2023-11-14 14:33:42 +0100

    Suppression interface inutile et imports

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,7 @@
+import React from 'react';
 import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
-import DesignInterface from '../DesignInterface/DesignInterface';
-import MaterialDesign from '../DesignInterface/MaterialDesign';
-import ReactNativePaper from '../DesignInterface/ReactNativePaper';
-
-const design: DesignInterface = new ReactNativePaper();
 
 export default function TabOneScreen() {
   return (
@@ -14,8 +9,6 @@ export default function TabOneScreen() {
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
-      {design.renderButton(() => {}, 'Cliquez-moi !')}
-      {design.renderCard('Titre', 'Contenu')}
     </View>
   );
 }
