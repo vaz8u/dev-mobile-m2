9a866cda6503fc0a6bd6d42a3fa0d231a85c70fe
SamvelMan <85001762+SamvelMan@users.noreply.github.com>
2023-12-12 22:45:32 +0100

    Modification Alarm List

--- components/AlarmList.tsx
+++ components/AlarmList.tsx
@@ -1,62 +1,62 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableWithoutFeedback } from 'react-native';
-import { List, Switch } from 'react-native-paper';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Avatar, Checkbox, List, Switch } from 'react-native-paper';
 
+export interface AlarmListInterface {
+    title: string,
+    description: string,
+    enable: boolean,
+    selected: boolean,
+    day: Day
+}
 
-export default function AlarmList() {
-    const [isSwitchOn, setIsSwitchOn] = React.useState(false);
-    const [isPressing, setIsPressing] = useState(false);
-    const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+export enum Day {
+    Lundi = 'L',
+    Mardi = 'Ma',
+    Mercredi = 'Me',
+    Jeudi = 'J',
+    Vendredi = 'V',
+    Samedi = 'S',
+    Dimanche = 'D',
+}
 
-    const handlePressIn = () => {
-        setIsPressing(true);
-    
-        // Définir le délai après lequel la fonction sera appelée (en millisecondes)
-        const delay = 2000; // 2 secondes
-    
-        // Utiliser setTimeout pour appeler la fonction après le délai
-        setTimeout(() => {
-          if (isPressing) {
-            // Appeler votre fonction ici
-            console.log('Fonction appelée après 2 secondes de pression');
-          }
-        }, delay);
-      };
-    
-      const handlePressOut = () => {
-        setIsPressing(false);
-      };
+const AlarmList = (props: AlarmListInterface & { isSelected: boolean, onUpdate: (key: keyof AlarmListInterface, newValue: any) => void  }) => {
+    const onToggleSwitch = () => { props.onUpdate('enable', !props.enable);};
 
     return (
-        <TouchableWithoutFeedback onPressIn={handlePressIn} onPressOut={handlePressOut}>
-            <List.Item 
-                style={styles.item}
-                title="List Item"
-                description="08:00"
-                left={props => <List.Icon icon="size-l" />}
-                right={props => <Switch style={styles.switch} value={isSwitchOn} onValueChange={onToggleSwitch}/>}
-            />
-        </TouchableWithoutFeedback>
+      <List.Item 
+            style={styles.item}
+            title={props.title}
+            description={props.description}
+            left={() => ( 
+                <Avatar.Text
+                    style={[styles.day]}
+                    label={props.day}
+                    size={35}
+                />
+            )}
+            right={() => (
+                props.isSelected ? (
+                    <Checkbox
+                        status={props.selected ? 'checked' : 'unchecked'}
+                        onPress={() => {props.onUpdate('selected', !props.selected);}}
+                    />
+                ) : (
+                    <Switch value={props.enable} onValueChange={onToggleSwitch}/>
+                )
+            )}
+      />
     );
 }
+
+export default AlarmList;
 
 const styles = StyleSheet.create({
     item:{
         width:'100%',
         padding: 10,
-        
     },
-    icon:{
-        width: 40,
-        height: 40,
-        backgroundColor: "red",
-        borderRadius: 50,
-        alignSelf:'center',
-        textAlign:'center',
-        textAlignVertical:'center'
-        
+    day:{
+        alignSelf:'center'
     },
-    switch:{
-
-    }
 });
