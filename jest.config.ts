97c01af93eb5c6df774bdc21871bb1039c16e0a2
Pierre VAZ <93267710+vaz8u@users.noreply.github.com>
2023-12-21 17:21:12 +0100

    Delete jest.config.ts

--- jest.config.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // Liste des chemins à ignorer pour la génération du coverage
-    coveragePathIgnorePatterns: [
-      './wake-up-api', // Chemin absolu vers le dossier à ignorer
-    ],
-  };
-  
