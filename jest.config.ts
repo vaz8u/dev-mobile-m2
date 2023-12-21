b7765a780966c22e5b529e30890af10671377309
Pierre VAZ <93267710+vaz8u@users.noreply.github.com>
2023-12-21 17:13:01 +0100

    Create jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+module.exports = {
+    // Liste des chemins à ignorer pour la génération du coverage
+    coveragePathIgnorePatterns: [
+      './wake-up-api', // Chemin absolu vers le dossier à ignorer
+    ],
+  };
+  
