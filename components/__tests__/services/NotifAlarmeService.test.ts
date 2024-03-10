32cda413a1ec8ac6f6bbfe89cf7e519287e9cff1
vaz8u <vaz.pierre57@gmail.com>
2024-03-10 17:42:33 +0100

    Delete NotifAlarmeService.test.ts

--- components/__tests__/services/NotifAlarmeService.test.ts
+++
@@ -1,4 +0,0 @@
-import * as Notifications from 'expo-notifications';
-import { schedulePushNotification } from './../../../services/NotifAlarmeService'; // Assurez-vous d'importer correctement le fichier contenant les fonctions à tester
-
-
