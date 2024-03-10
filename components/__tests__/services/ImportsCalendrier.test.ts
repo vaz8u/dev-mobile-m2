4a23ea6715bd83002fd08bdff8e01910a104191d
vaz8u <vaz.pierre57@gmail.com>
2024-03-10 17:33:51 +0100

    Tests imports

--- components/__tests__/services/ImportsCalendrier.test.ts
+++ components/__tests__/services/ImportsCalendrier.test.ts
@@ -1,129 +1,153 @@
-import { getLienEDT_FAC, convertIcsToJson, creerElementsCalendrierFromJson, getCalendriers, getAndSetCalendriers, afficherCalendriersTrouvees, afficherCalendriersTrouveesAvecViewText, importManuel } from '../../../services/ImportsCalendrier';
-import RNCalendarEvents, { CalendarEventReadable } from 'react-native-calendar-events';
-import DocumentPicker, { DocumentPickerOptions } from 'expo-document-picker';
+import { Calendrier, Evenement } from '../../../models/Evenement';
+import { getLienEDT_FAC, convertIcsToJson, creerElementsCalendrierFromJson, getCalendriers, getAndSetCalendriers,afficherCalendriersTrouvees, afficherCalendriersTrouveesAvecViewText, importManuel, addCalendrier, deleteCalendrier } from '../../../services/ImportsCalendrier';
+import * as DP from 'expo-document-picker';
 
-describe('getLienEDT_FAC', () => {
-  it('retourne le bon lien pour une classe donnée', () => {
-    const classe = 'ihm'; 
-    const expectedLink = 'https://planning.univ-lorraine.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=158863&projectId=11&calType=ical&nbWeeks=20';
+// Test pour la fonction getLienEDT_FAC
+test('getLienEDT_FAC', () => {
+    const classe = 'SSI';
+    const result = getLienEDT_FAC(classe);
+    expect(result).toBeDefined();
+});
+const fileContent = `BEGIN:VCALENDAR
+METHOD:PUBLISH
+PRODID:-//ADE/version 6.0
+VERSION:2.0
+CALSCALE:GREGORIAN
+BEGIN:VEVENT
+DTSTAMP:20240207T161821Z
+DTSTART:20240208T073000Z
+DTEND:20240208T113000Z
+SUMMARY:' PROJETS -  Intégration méthodologique IHM
+LOCATION:Technopole_BN2-013
+DESCRIPTION:\\n\\nTD Intégration méthodologique IHM\\nTD Intégration méthodo
+ logique IHM\\nTD Intégration méthodologique IHM\\nTD Intégration méthodolo
+ gique IHM\\nTD Intégration méthodologique IHM\\nTD Intégration méthodologi
+ que IHM\\nTD Intégration méthodologique IHM\\nEnseignant 1\\n\\n(Modifié le:
+ 19/12/2023 12:01)
+UID:ADE60322e706c616e6e696e67554c5f323032335f323032342d3133333534362d332d
+ 30
+CREATED:19700101T000000Z
+LAST-MODIFIED:20231219T110100Z
+SEQUENCE:2141033201
+END:VEVENT
+END:VCALENDAR`;
 
-    expect(getLienEDT_FAC(classe)).toEqual(expectedLink);
-  });
+jest.mock('react-native-fs', () => ({
+  readFile: jest.fn(() => Promise.resolve(fileContent)),
+}));
+
+// Test pour la fonction convertIcsToJson
+test('convertIcsToJson', async () => {
+    const _data = '../../../assets/test.ics'; // Remplacez 'lienTest' par un lien valide
+    const result = await convertIcsToJson(_data);
+    expect(result).toBeDefined();
 });
 
-/*
-describe('convertIcsToJson', () => {
-  it('convertit correctement les données ICS en JSON', async () => {
-    const data = '../../../assets/test.ics';
-    const expectedJson = [
+// Test pour la fonction creerElementsCalendrierFromJson
+test('creerElementsCalendrierFromJson', () => {
+    const json = [
         {
-          description: `
-      TD Intégration méthodologique IHM
-      Enseignant 1
-      
-      (Modifié le:19/12/2023 12:01)`,
-          end: "2024-02-08T11:30:00Z",
-          location: "Technopole_BN2-013",
-          start: "2024-02-08T07:30:00Z",
-          summary: "' PROJETS -  Intégration méthodologique IHM"
-        }
-      ];
-    const result = await convertIcsToJson(data);
-    expect(result).toEqual(expectedJson);
-  });
-});*/
-
-describe('creerElementsCalendrierFromJson', () => {
-    it('crée correctement les éléments du calendrier à partir du JSON', () => {
-      const json = [
+            "summary": "' PROJETS -  Intégration méthodologique IHM",
+            "start": "2024-02-08T07:30:00.000Z",
+            "end": "2024-02-08T11:30:00.000Z",
+            "location": "Technopole_BN2-013",
+            "description": "TD Intégration méthodologique IHM"
+        },
         {
-          summary: 'Résumé de test',
-          start: '2024-02-08T07:30:00Z',
-          end: '2024-02-08T11:30:00Z',
-          location: 'Location de test',
-          description: 'Description de test'
+            "summary": "Réunion d'équipe",
+            "start": "2024-02-09T09:00:00.000Z",
+            "end": "2024-02-09T10:00:00.000Z",
+            "location": "Salle de conférence",
+            "description": "Réunion hebdomadaire de l'équipe de projet"
         }
-      ];
-      const expectedElements = [
-        {
-          titre: 'Résumé de test',
-          startDate: new Date('2024-02-08T07:30:00Z'),
-          endDate: new Date('2024-02-08T11:30:00Z'),
-          location: 'Location de test',
-          description: 'Description de test',
-          calendrier: 'EDT'
-        }
-      ];
-  
-      expect(creerElementsCalendrierFromJson(json)).toEqual(expectedElements);
-    });
-  });
-  
-  describe('getCalendriers', () => {
-    it('retourne les calendriers détectés dans le téléphone', async () => {
-      jest.mock('react-native-calendar-events', () => ({
-        requestPermissions: jest.fn(() => Promise.resolve(true)),
-        checkPermissions: jest.fn(() => Promise.resolve(true)),
-        findCalendars: jest.fn(() => Promise.resolve([{ title: 'Calendrier de test' }])),
-        fetchAllEvents: jest.fn(() => Promise.resolve([]))
-      }));
-  
-      const expectedCalendars = ['Calendrier de test'];
-  
-      const result = await getCalendriers();
-      expect(result).toEqual(expectedCalendars);
-    });
-  });
-
-  jest.mock('react-native-calendar-events');
-  jest.mock('react-native-document-picker', () => ({
-    pick: jest.fn(),
+    ]
+    ;
+    const result = creerElementsCalendrierFromJson(json);
+    expect(result).toBeDefined();
+});
+jest.mock('react-native-calendar-events', () => ({
+    requestPermissions: jest.fn(() => Promise.resolve(true)),
+    checkPermissions: jest.fn(() => Promise.resolve(true)),
+    findCalendars: jest.fn(() => Promise.resolve([])),
+    fetchAllEvents: jest.fn(() => Promise.resolve([])),
   }));
   
-  describe('Test des fonctions', () => {
-    test('getAndSetCalendriers', async () => {
-            const events: CalendarEventReadable[] = [{
-                id: '1',
-                title: 'Mon événement',
-                startDate: '2024-01-01T00:00:00.000Z',
-                calendar: { 
-                    id: '1', 
-                    type: 'local', 
-                    source: '', 
-                    isPrimary: true, 
-                    title: 'Mon Calendrier',
-                    allowsModifications: true,
-                    color: '#FFFFFF',
-                    allowedAvailabilities: []
-                }
-            }];
-            const mockedFetchAllEvents = RNCalendarEvents.fetchAllEvents as jest.MockedFunction<typeof RNCalendarEvents.fetchAllEvents>;
-            mockedFetchAllEvents.mockResolvedValue(events);
-            const result = await getAndSetCalendriers('Mon Calendrier');
-            expect(result).toBeDefined();
-            // Ajoutez plus d'assertions en fonction de ce que la fonction addCalendrier est censée retourner
-    });
-    
+// Test pour la fonction getCalendriers
+test('getCalendriers', async () => {
+    const result = await getCalendriers();
+    expect(result).toBeDefined();
+});
+
+// Test pour la fonction getAndSetCalendriers
+test('getAndSetCalendriers', async () => {
+    const nomCalendrier = 'calendrierTest';
+    const result = await getAndSetCalendriers(nomCalendrier);
+    expect(result).toBeDefined();
+});
+
+// Mock pour la classe Evenement
+// Mock pour la classe Evenement
+const mockEvenement = new Evenement(
+    "' PROJETS -  Intégration méthodologique IHM",
+    new Date('2024-02-08T07:30:00Z'),
+    new Date('2024-02-08T11:30:00Z'),
+    'Technopole_BN2-013',
+    "\nTD Intégration méthodologique IHM\nEnseignant 1\n(Modifié le:19/12/2023 12:01)",
+    'EDT'
+);
+
+// Mock pour la classe Calendrier
+const mockCalendrier = new Calendrier(
+    [mockEvenement],
+    'fileName',
+    'blue'
+);
+
+// Test pour la fonction afficherCalendriersTrouvees
+test('afficherCalendriersTrouvees', () => {
+    const calendriers = [mockCalendrier];
+    const events = [mockEvenement, mockEvenement];
+     // Créer un espion sur console.log
+     const spy = jest.spyOn(console, 'log');
+    afficherCalendriersTrouvees(calendriers, events);
+        // Vérifier que console.log a été appelé
+        expect(spy).toHaveBeenCalled();
+
+        // Restaurer console.log à son comportement original
+        spy.mockRestore();
+});
+
+// Test pour la fonction afficherCalendriersTrouveesAvecViewText
+test('afficherCalendriersTrouveesAvecViewText', () => {
+    const result = afficherCalendriersTrouveesAvecViewText(mockCalendrier, mockEvenement);
+    expect(result).toBeDefined();
+});
+
+// Test pour la fonction importManuel
+jest.mock('expo-document-picker', () => ({
+    getDocumentAsync: jest.fn(),
+  }));
   
-    test('afficherCalendriersTrouvees', () => {
-      console.log = jest.fn();
-      const calendars = [{ title: 'Mon Calendrier', source: 'Source', events: [] }];
-      const events = [{ title: 'Mon événement', source: 'Source' }];
-      afficherCalendriersTrouvees(calendars, events);
-      expect(console.log).toHaveBeenCalled();
-    });
-  /*
-    test('importManuel', async () => {
-      const files: DocumentPickerOptions[] = [{ 
-        uri: 'file://monFichier.ics', 
-        name: 'Mon Calendrier',
-        fileCopyUri: '',
-        type: '',
-        size: 0
-      }];
-      const mockedPick = DocumentPicker.pick as jest.MockedFunction<typeof DocumentPicker.pick>;
-      mockedPick.mockResolvedValue(files);
+  test('importManuel', async () => {
+      const mockFile = { assets: [{ uri: 'fileUri', name: 'fileName' }] };
+  
+      (DP.getDocumentAsync as jest.Mock).mockResolvedValue(mockFile);
+  
       const result = await importManuel();
-      expect(result).toBeDefined();
-    });*/
-  });
+  
+      expect(result).toEqual([mockCalendrier]);
+      expect(DP.getDocumentAsync).toHaveBeenCalledWith({ type: 'text/calendar' });
+    }); 
+
+// Test pour la fonction addCalendrier
+test('addCalendrier', async () => {
+    const result = await addCalendrier(mockCalendrier);
+    expect(result).toBeTruthy();
+});
+
+// Test pour la fonction deleteCalendrier
+test('deleteCalendrier', async () => {
+    const nomCalendrier = 'calendrierTest';
+    const result = await deleteCalendrier(nomCalendrier);
+    expect(result).toBeTruthy();
+});
