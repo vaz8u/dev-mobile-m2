1a0686220847f572fd332c0cd5e9b6feafb7bdb1
SamvelMan <85001762+SamvelMan@users.noreply.github.com>
2024-03-20 14:17:31 +0100

    ScrollView react native paper

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -3,7 +3,7 @@
  * https://docs.expo.io/guides/color-schemes/
  */
 
-import { Text as DefaultText, useColorScheme, View as DefaultView } from "react-native";
+import { Text as DefaultText, useColorScheme, View as DefaultView, ScrollView as DefaultScrollView } from "react-native";
 import { useTheme } from "react-native-paper";
 
 import Colors from "../constants/Colors";
@@ -16,6 +16,7 @@ type ThemeProps = {
 
 export type TextProps = ThemeProps & DefaultText["props"];
 export type ViewProps = ThemeProps & DefaultView["props"];
+export type ScrollViewProps = ThemeProps & DefaultView["props"];
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
@@ -51,4 +52,12 @@ export function View(props: ViewProps) {
   const backgroundColor = lightColor && darkColor ? (theme.dark ? darkColor : lightColor) : theme.colors.surfaceVariant; 
 
   return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+}
+
+export function ScrollView(props: ScrollViewProps) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const theme = useTheme(); 
+  const backgroundColor = lightColor && darkColor ? (theme.dark ? darkColor : lightColor) : theme.colors.surfaceVariant; 
+
+  return <DefaultScrollView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
